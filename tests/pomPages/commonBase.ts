6fa1cc69760b3137584ffa217f60bf89d76b0066
saiteja-sureify <saiteja.peravali@sureify.com>
2024-02-01 12:18:24 +0530

    Commiting the changes made till 01/02 12:30pm

--- tests/pomPages/commonBase.ts
+++ tests/pomPages/commonBase.ts
@@ -1,4 +1,8 @@
+import path from "path";
 import { Page } from "playwright";
+import { signincolumns } from "./signIn";
+import fs from "fs";
+import { parse } from "csv-parse/sync";
 
 export default class COMMONBASE {
   protected page: Page;
@@ -9,6 +13,36 @@ export default class COMMONBASE {
 
   async navigate(url: string): Promise<void> {
     await this.page.goto(`${url}`);
+    await this.loadStateDomContent();
+  }
+
+  async loadStateDomContent(): Promise<void> {
     await this.page.waitForLoadState("domcontentloaded");
   }
+
+  async loadStateNetworkIdle(): Promise<void> {
+    await this.page.waitForLoadState("networkidle");
+  }
+
+  async loadState(): Promise<void> {
+    await this.page.waitForLoadState("domcontentloaded");
+    await this.page.waitForLoadState("networkidle");
+    await this.page.waitForLoadState("load");
+  }
 }
+
+//CSV File Path and Declaration for Sign In Screen
+
+const root_dir = process.cwd();
+console.log(root_dir);
+export let fileSep = path.sep;
+console.log(fileSep);
+export const tessDataFolder: string =
+  root_dir + fileSep + "tests" + fileSep + "testData_Csv";
+console.log("Directory in Common Base: ", tessDataFolder);
+
+export const signinData: string = tessDataFolder + fileSep + "luma_signin.csv";
+console.log("Directory of CSV File: ", signinData);
+
+export const csvFilePath = path.resolve(__dirname, signinData);
+export const fileContent = fs.readFileSync(csvFilePath, { encoding: "utf-8" });
--- tests/pomPages/signIn.ts
+++ tests/pomPages/signIn.ts
@@ -1,18 +1,108 @@
 import { Page } from "playwright";
 import COMMONBASE from "./commonBase";
 
+interface signinrecords {
+  existing_user: string;
+  eu_username: string;
+  eu_password: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+}
+
+export const signincolumns = [
+  "existing_user",
+  "eu_username",
+  "eu_password",
+  "first_name",
+  "last_name",
+  "email",
+  "password",
+];
+
 export class SIGNINPAGE extends COMMONBASE {
   constructor(page: Page) {
     super(page);
   }
 
-  SignINButton = () =>
+  SignInButtonHome = () =>
     this.page.locator(
       "//div[@class='panel header']//a[contains(text(),'Sign In')]"
     );
 
-  async ClickingTheSignInButton(): Promise<void> {
-    await this.SignINButton().click();
-    await this.page.pause();
+  EmailTextBox = () =>
+    this.page.locator("//div[@class='control']/input[@title='Email']");
+
+  PasswordTextBox = () =>
+    this.page.locator("//div[@class='control']/input[@title='Password']");
+
+  SignInButtonLocator = () =>
+    this.page.locator(
+      "//div[@class='primary']/button[@class='action login primary']/span[contains(text(), 'Sign In')]"
+    );
+
+  FirstNameTextbox = () =>
+    this.page.locator("//div[@class='control']/input[@name='firstname']");
+
+  LastNameTextbox = () =>
+    this.page.locator("//div[@class='control']/input[@name='lastname']");
+
+  EmailTextbox = () =>
+    this.page.locator("//div[@class='control']/input[@name='email']");
+
+  PasswordTextbox = () =>
+    this.page.locator("//div[@class='control']/input[@name='password']");
+
+  ConfirmPasswordTextbox = () =>
+    this.page.locator(
+      "//div[@class='control']/input[@name='password_confirmation']"
+    );
+
+  CreateAnAccountSignInForm = () =>
+    this.page.locator(
+      "//a[@class='action create primary']/span[contains(text(),'Create an Account')]"
+    );
+
+  CreateAnAccountFormButton = () =>
+    this.page.locator(
+      "//button[@class='action submit primary']/span[contains(text(),'Create an Account')]"
+    );
+
+  async SignIn_SignUp({
+    existing_user,
+    eu_password,
+    eu_username,
+    first_name,
+    last_name,
+    email,
+    password,
+  }: signinrecords): Promise<void> {
+    if (existing_user.trim().toLocaleLowerCase() == "yes") {
+        console.log("Existing User Sign In");
+      await this.SignInButtonHome().click();
+      await this.loadStateDomContent();
+
+      await this.EmailTextBox().fill(eu_username);
+      await this.PasswordTextBox().fill(eu_password);
+      await this.SignInButtonLocator().click();
+      await this.loadStateDomContent();
+      console.log("Existing User Logged in successfully");
+    } else {
+        console.log("New User Account Creation");
+      await this.SignInButtonHome().click();
+      await this.loadStateDomContent();
+
+      await this.CreateAnAccountSignInForm().click()
+      await this.FirstNameTextbox().fill(first_name);
+      await this.LastNameTextbox().fill(last_name);
+      await this.EmailTextBox().fill(email);
+      await this.PasswordTextBox().fill(password);
+      await this.ConfirmPasswordTextbox().fill(password);
+      
+      await this.CreateAnAccountFormButton().click();
+      await this.loadStateDomContent();
+      console.log("New User Account Creation completed successfully");
+    }
   }
 }
